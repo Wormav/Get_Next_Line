5deef104ace4117c2189001d6699c5249310890e
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-22 23:59:53 +0200

    sort function in header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 12:50:53 by jlorette          #+#    #+#             */
-/*   Updated: 2024/08/22 23:54:01 by jlorette         ###   ########.fr       */
+/*   Updated: 2024/08/22 23:59:30 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-int		search_new_line(t_list *storage);
-t_list	*search_last_node(t_list *storage);
-void	free_storage(t_list *storage);
+void	add_storage(t_list **storage, char *buffer, int read_return);
 void	alloc_line(char **line, t_list *storage);
 char	*copy_content_after_newline(char *content, int start);
-char	*get_next_line(int fd);
-void	add_storage(t_list **storage, char *buffer, int read_return);
-void	read_and_stock(int fd, t_list **storage);
 void	create_line(t_list *storage, char **line);
+void	free_storage(t_list *storage);
+char	*get_next_line(int fd);
+void	read_and_stock(int fd, t_list **storage);
 void	reset_storage(t_list **storage);
+int		search_new_line(t_list *storage);
+t_list	*search_last_node(t_list *storage);
 
 #endif
